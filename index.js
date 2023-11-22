1ff1c69d598ff747241d7bd8dfb9fe6a32c2c00f
ignatiusgabrieltan <ignatiusgabrieltanwei@gmail.com>
2023-11-23 02:14:26 +0800

    edited cors

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const app = express();
 
 //-----------Enable CORS access to this server-----------//
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://ultralock.netlify.app'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
