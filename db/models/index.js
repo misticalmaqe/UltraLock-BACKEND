d8b5b96c4eef68d5c57fa7caa36ae8af0c02e8b2
root <khaira_jaipal@hotmail.com>
2023-11-28 13:33:22 +0800

    Continuous integration deployment

--- db/models/index.js
+++ db/models/index.js
@@ -1,18 +1,28 @@
-'use strict';
+"use strict";
 
 //importing everything we need && initilizing variables we will ultimately pass out (db)
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/database.js')[env];
+const env = process.env.NODE_ENV || "production";
+const config = require(__dirname + "/../../config/database.js")[env];
 const db = {};
-
+require("dotenv").config();
 //Develops the connection into the DB
 let sequelize;
-if (config.use_env_variable) {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.USERNAME,
+    process.env.PASSWORD,
+    {
+      host: process.env.HOST,
+      dialect: process.env.DIALECT,
+    }
+  );
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
@@ -27,10 +37,10 @@ if (config.use_env_variable) {
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach((file) => {
